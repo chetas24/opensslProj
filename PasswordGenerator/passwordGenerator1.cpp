5dcb392520a808b13dcb6740c18e188c33d3c44e
chetas24 <barhatechetas@gmail.com>
2023-07-13 20:24:32 +0530

    Password Generator

---
+++ PasswordGenerator/passwordGenerator1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <openssl/rand.h>
+#include <cstring>
+
+
+std::string generateRandomPassword(int length) {
+    const std::string valid_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$";
+
+    unsigned char buffer[length];
+    std::string password;
+
+    // Generate random bytes
+    if (RAND_bytes(buffer, sizeof(buffer)) != 1) {
+        std::cerr << "Error generating random numbers." << std::endl;
+        return password;
+    }
+
+    // Convert random bytes to password characters
+    for (int i = 0; i < length; ++i) {
+        unsigned char randomChar = buffer[i] % valid_chars.length();
+        password += valid_chars[randomChar];
+    }
+
+    return password;
+}
+
+int main() {
+    int passwdLength = 10;
+
+    std::string password = generateRandomPassword(passwdLength);
+    std::cout << "Generated Random Password: " << password << std::endl;
+
+    return 0;
+}
